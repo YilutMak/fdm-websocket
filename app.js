3f5807e76293d7dcf46471f213c45add0ac44bb7
Mak <yilut.mak@fdmgroup.com>
2024-03-26 15:13:07 +0800

    added program start script

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
     methods: ["GET", "POST"]
   }
 });
